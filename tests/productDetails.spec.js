43f5c0b9ba198dcbe8938ee0413076d7d5834d3a
Felipe Pinto <felipe14062000@gmail.com>
2022-07-19 18:06:50 -0300

    Resolução do requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,14 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(typeof productDetails).toBe('function');
+    expect(Array.isArray(productDetails('alcool', 'luvas'))).toBeTruthy();
+    expect(productDetails('luvas', 'cotonete')).toHaveLength(2);
+    expect(typeof productDetails('prato', 'mesa')[0]).toBe('object');
+    expect(typeof productDetails('prato', 'mesa')[1]).toBe('object');
+    expect(productDetails('luva', 'alcool em gel')[0]).not.toEqual(productDetails('luva', 'alcool em gel')[1]);
+    expect(productDetails('prato', 'mesa')[0].details.productId).toMatch('123');
+    expect(productDetails('prato', 'mesa')[1].details.productId).toMatch('123');
     // Teste se os dois productIds terminam com 123.
   });
 });
